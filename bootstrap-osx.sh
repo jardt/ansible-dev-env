d46e88ac4abc3b2510d04ae30c2a0a13b2a01fa0
Jardar <jardar.ton@bouvet.no>
2025-04-03 17:46:33 +0200

    first commit

---
+++ bootstrap-osx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v brew)" ]; then
+echo "brew command not found.. installing homebrew"
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+fi
+
+echo "installing ansible"
+brew list ansible || brew install ansible
+
+echo "installing stow"
+brew list stow || brew install stow
+
+
